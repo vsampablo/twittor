82f49a459ef94857625f27656e307aa0f71007b1
Victor Sampablo <vsampablo@develsuit.com>
2021-07-11 09:42:39 -0500

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -43,6 +43,11 @@ self.addEventListener('activate', e=>{
             if (key !== STATIC_CACHE && key.includes('static')){
                 return caches.delete(key);
             }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')){
+                return caches.delete(key);
+            }
+
         });
     });
     e.waitUntil(respuesta);
