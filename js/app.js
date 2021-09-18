263155ad246f83de554ba9bc1e3260ec78bb217f
Victor Sampablo <vsampablo@develsuit.com>
2021-09-17 22:50:06 -0500

    Install pwa

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 let url = window.location.href;
 let swLocation = '/twittor/sw.js';
 
+// Initialize deferredPrompt for use later to show browser install prompt.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+
 if (navigator.serviceWorker){
     if (url.includes('localhost')){
         swLocation = '/sw.js';
